01a0e91fb7f863a6b1e586a19eb8ae0fc77a29d7
Rasheem Khan <106111818+Rvester@users.noreply.github.com>
2022-08-10 16:19:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,156 @@
+
+
+let firstname = "Rasheem";
+let lastname = "Khan";
+let fullname = lastname + firstname;
+
+console.log(fullname)
+
+
+
+let x= 5;
+let y= 8;
+let result = x + y;
+
+console.log (result);
+
+
+
+let num1 = 12;
+let num2 = 15;
+let answer = x * y;
+
+console.log(answer);
+
+
+let num3 = 12;
+let num4 =15;
+let num5 = 16;
+let num6 =22;
+let num7 = 30;
+let num8 = 40;
+let num9 = 45;
+let num10 = 33;
+let num11 = 27;
+let num12 = 28;
+
+let firstaverage = ((num3+num4+num5+num6+num7)/5)
+let secondaverage= ((num8+num9+num10+num11+num12)/5)
+
+console.log(firstaverage)
+console.log(secondaverage)
+console.log((firstaverage+secondaverage)/2)
+
+
+findaverage = ((4,10,22,-30,55)/5)
+findaverage2 = ((68,7,-22,9,100)/5)
+console.log(findaverage)
+console.log(findaverage2)
+
+let box1 = "cookies";
+let box2 = "cake";
+let box3 
+
+	 
+
+
+
+const number = -3
+if (number > 0) {
+    console.log("The number is positive")
+}
+ else if (number == 0){
+    console.log("The number is zero")
+ }
+else {
+    console.log("The number is negative")
+}
+
+let age = 18; 
+ 
+if (age >= 18){
+    console.log("You are permitted to enter")
+
+ }
+ else if (age < 18){
+    console.log("get out")
+ }
+
+
+ let num = 120;
+ if (num <=100 && num >0){
+    console.log("number is positive but less than one-hundred")
+
+ }
+ else if (num > 100){
+    console.log("number is positive and greater than one-hundred")
+ }
+else if (num < 0){
+    console.log("number is negative and less than 100")
+
+}
+
+
+
+
+let grade = 98
+if (grade > 90){
+    console.log("A+")
+
+}
+else if (grade <=90){
+    console.log("A")
+
+}
+else if (grade >80 && num<=89){
+    console.log("B")
+}
+else if (grade >70 && num <=79){
+    console.log("C")
+
+}
+else if (grade >55 && num <=69){
+    console.log("D")
+}
+else{
+    console.log("See me after class")
+}
+
+
+let letter = "A";
+
+    switch (letter) {
+    case "A":
+    case "B":
+    case "C":
+    case "D":    
+      
+       console.log("vowel")
+      
+
+        
+        break;
+
+    default:
+        console.log("consonant")
+        break;
+}
+
+
+let num25 = 1;
+   
+let status = num25 > 0 ? "postive" : "negative";
+
+let num26 = 0;
+
+let status2 = num26 === 0 ? "Zero" : "Not Zero"
+
+
+   
+
+
+
+
+
+
+
